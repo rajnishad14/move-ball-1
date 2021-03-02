b307b38429dff57367571432654b70307e4b3528
rajnishad14 <raj.nishad14@gmail.com>
2021-03-02 14:25:20 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+    const [rendelBall,setRenderBall] = useState(false);
+    //const [posi,setPosi] = useState(0);
+    const [ballPosition, setBallPosition] = useState({left:0 , top:0});
+
+    function buttonClickHandler(){
+        setRenderBall(true);
+    }
+    useEffect(()=>{
+        const keyListner=document.addEventListener('keydown',(e)=>{
+            switch(e.keyCode){
+                case 37:
+                    setBallPosition((ballPosition)=>{
+                        return {
+                            left:ballPosition.left-5,
+                            top:ballPosition.top
+                        }
+                    });
+                    break;
+                case 38:
+                    setBallPosition((ballPosition)=>{
+                        return {
+                            left:ballPosition.left,
+                            top:ballPosition.top-5
+                        }
+                    });
+                    break;
+                case 39:
+                    setBallPosition((ballPosition)=>{
+                        return {
+                            left:ballPosition.left+5,
+                            top:ballPosition.top
+                        }
+                    });
+                    break;
+                case 40:
+                    setBallPosition((ballPosition)=>{
+                        return {
+                            left:ballPosition.left,
+                            top:ballPosition.top+5
+                        }
+                    });
+                    break;
+                default:
+                  break;
+            }
+
+        });
+        return (()=>{document.removeEventListener(keyListner,"keydown");});
+    },[]);
+
+    function RenderBallOrButton(){
+        if(rendelBall){
+            return <div className="ball"
+            style={{
+                top:ballPosition.top+"px",
+                left:ballPosition.left+"px",
+                position:"absolute"
+
+            }}
+            ></div>
+        }else{
+            return <button onClick={buttonClickHandler}>Click For One Ball</button>
+        }
+    }
+
+    function ReturnDiv(){
+        return(
+            <div className="playground"><RenderBallOrButton/></div>
+        );
+    }
+    
+    return(
+        <ReturnDiv/>
+    );
+    
 }
+
 
 export default App;
